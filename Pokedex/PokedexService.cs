bbfb3d2242349da974fea151a69f142daacc2d79
adgo godoy anubla <adgo666@hotmail.com>
2023-02-26 23:56:13 +0100

    non compiling comit

--- Pokedex/PokedexService.cs
+++ Pokedex/PokedexService.cs
@@ -1,4 +1,5 @@
-﻿using Pokedex.Repositories;
+﻿using Pokedex.Models;
+using Pokedex.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,9 +17,39 @@ namespace Pokedex
             _pokedexRepository = pokedexRepository;
         }
 
-        public Task<string> GetPokemonsTypeStrenghtsAndWeaknesses(string id) { 
+        public async Task<string> GetPokemonsTypeStrenghtsAndWeaknesses(string id)
+        { 
+           var pokemon= await _pokedexRepository.GetPokemonByIdOrName(id);
+
+           await pokemon.Match(  ProcessPokemonStrenghtsAndWeaknesses,  ProcessError);
         
-        
+        }
+
+
+        public async Task<string> ProcessPokemonStrenghtsAndWeaknesses(Pokemon pokemon)
+        {
+           var typesResult=  pokemon.types.AsParallel().Select( async type => await _pokedexRepository.GetTypeByIdOrName(type.type.Name)).ToList();
+            if (typesResult.Any( result =>  result.Result.IsT1)) {
+
+               return await ProcessError(typesResult.First(result => result.Result.IsT1).Result.AsT1);
+            }
+            var damageRelations=typesResult.Select(oneOf => oneOf.Result.AsT0).Select(type => type.DamageRelations);
+
+            var DamageMultipliyerDiccionary = new Dictionary<string, (int,int)>();
+            foreach (var damageRelation in damageRelations)
+            {
+                foreach( var type in damageRelation.HalfDamageFrom)
+                {
+                    
+                }
+            }
+            
+
+        }
+
+        public async Task<string> ProcessError(PokedexError error)
+        {
+
         }
     }
 }
